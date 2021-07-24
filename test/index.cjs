e1dfe88a5a6c5c39f3706053014db0fe8fcd1208
Eric Chen <airkro@qq.com>
2021-07-24 18:43:54 +0800

    fix: bug

--- test/index.cjs
+++ test/index.cjs
@@ -6,8 +6,18 @@ const { format } = require('prettier');
 const original = `{
   "name": "prettier-plugin-package-json",
   "private": true,
+
+    "author": "abc<abc@example.com>(http://example.com)",
+
   "main": "./index.js",
-  "license": "unlicensed"
+  "typings":"",
+  "preferGlobal": false,
+  "license": "unlicensed",
+"repository":{
+  "url": "","directory":"m","type": ""
+},"resolutions":{
+  "b":"","a":""
+},"engines":{"node":"d ||    < 5"}
 }
 `;
 
@@ -16,7 +26,25 @@ const expected = `{
   "name": "prettier-plugin-package-json",
   "version": "0.0.0",
   "license": "UNLICENSED",
-  "main": "index.js"
+  "author": {
+    "name": "abc",
+    "email": "abc@example.com",
+    "url": "http://example.com"
+  },
+  "repository": {
+    "type": "",
+    "url": "",
+    "directory": "m"
+  },
+  "types": "",
+  "main": "index.js",
+  "resolutions": {
+    "a": "",
+    "b": ""
+  },
+  "engines": {
+    "node": "d || <5"
+  }
 }`;
 
 test('Usage', (t) => {
